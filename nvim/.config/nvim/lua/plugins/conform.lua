e197cccaf9fea1c6cf4958cb475d24292396ee22
Jay Pancholi <jaypancholi94@gmail.com>
2026-03-20 11:49:39 +1100

    refactor(nvim): migrate eslint from conform to lsp with flat config support

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -2,10 +2,6 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      javascript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescript = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
       markdown = { "prettier" },
     },
   },
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -6,12 +6,31 @@ return {
       astro = {},
       eslint = {
         settings = {
-          codeActionOnSave = {
-            enable = true,
-            mode = "all",
+          experimental = {
+            useFlatConfig = true,
           },
         },
       },
+    },
+    setup = {
+      eslint = function()
+        vim.api.nvim_create_autocmd("LspAttach", {
+          callback = function(args)
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client and client.name == "eslint" then
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = args.buf,
+                callback = function()
+                  local ok, err = pcall(vim.cmd, "LspEslintFixAll")
+                  if not ok then
+                    vim.notify("LspEslintFixAll failed: " .. tostring(err), vim.log.levels.WARN)
+                  end
+                end,
+              })
+            end
+          end,
+        })
+      end,
     },
   },
 }
