960af1d0a434b0af31bf364f6d3c85ca622fbc9b
Jay Pancholi <jaypancholi94@gmail.com>
2026-03-20 13:11:40 +1100

    feat(theme): migrate to duckbones colorscheme across all tools

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,6 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = "night",
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-  {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "tokyonight" },
+    opts = { colorscheme = "duckbones" },
   },
 }
